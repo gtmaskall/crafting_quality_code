23e026814b1a3ca73c5c4cdb49609dac798f7597
Dr Guy Maskall <guymaskall@gmail.com>
2018-08-18 17:25:50 +0100

    Implement Rat methods
    Implemented __init__, set_location, eat_sprout, __str__

--- week_5_rat_game/a2.py
+++ week_5_rat_game/a2.py
@@ -37,8 +37,83 @@ class Rat:
     """ A rat caught in a maze. """
 
     # Write your Rat methods here.
+    def __init__(self, symbol, row, col):
+        """(Rat, str, int, int) -> NoneType
+
+        Initialise a Rat
+
+        >>> rat = Rat('P', 2, 3)
+        >>> rat.symbol
+        'P'
+        >>> rat.row
+        2
+        >>> rat.col
+        3
+        >>> rat.num_sprouts_eaten
+        0
+        """
+
+        self.symbol = symbol
+        self.row = row
+        self.col = col
+        self.num_sprouts_eaten = 0
+
+    def set_location(self, row, col):
+        """(Rat, int, int) -> NoneType
+
+        Set a Rat's location (row and column index)
+
+        >>> rat = Rat('P', 4, 5)
+        >>> rat.set_location(7, 8)
+        >>> rat.row
+        7
+        >>> rat.col
+        8
+        """
+
+        self.row = row
+        self.col = col
+
+    def eat_sprout(self):
+        """(Rat) -> NoneType
+
+        Increment number of sprouts eaten by 1
+
+        >>> rat = Rat('P', 3, 4)
+        >>> rat.num_sprouts_eaten
+        0
+        >>> rat.eat_sprout()
+        >>> rat.num_sprouts_eaten
+        1
+        """
+
+        self.num_sprouts_eaten += 1
+
+    def __str__(self):
+        """(Rat) -> str
+
+        Pretty print a Rat
+
+        >>> rat = Rat('S', 2, 3)
+        >>> print(rat)
+        S at (2, 3) ate 0 sprouts.
+        >>> rat.eat_sprout()
+        >>> rat.eat_sprout()
+        >>> print(rat)
+        S at (2, 3) ate 2 sprouts.
+        """
+
+        result = self.symbol + ' at (' + str(self.row) + ', ' + str(self.col) + \
+                ') ate ' + str(self.num_sprouts_eaten) + ' sprouts.'
+
+        return result
+
 
 class Maze:
     """ A 2D maze. """
 
     # Write your Maze methods here.
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
