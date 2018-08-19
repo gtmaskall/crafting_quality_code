dc4d36bf74af42bcbb8e31e75b549d506d073fc8
Dr Guy Maskall <guymaskall@gmail.com>
2018-08-19 15:19:26 +0100

    Implement final Maze methods

--- week_5_rat_game/a2.py
+++ week_5_rat_game/a2.py
@@ -134,11 +134,18 @@ class Maze:
         '@'
         >>> maze.rat_1.row
         1
+        >>> maze.num_sprouts_left
+        1
         """
 
         self.maze = maze
         self.rat_1 = rat_1
         self.rat_2 = rat_2
+        self.num_sprouts_left = 0
+
+        for item in sum(self.maze, []):
+            if item == SPROUT:
+                self.num_sprouts_left += 1
 
     def is_wall(self, row, col):
         """(Maze, int, int) -> bool
@@ -161,6 +168,101 @@ class Maze:
         """
 
         return self.maze[row][col] == WALL
+
+    def get_character(self, row, col):
+        """(Maze, int, int) -> str
+
+        Return the character in the maze at the given row (row) and 
+        column (col). If there's a rat at that location, return its character.
+
+        >>> maze = Maze([['#','#','#'],\
+                         ['#','.','#'],\
+                         ['#','@','#'],\
+                         ['#','#','#']],\
+                         Rat('J', 1, 1),\
+                         Rat('G', 2, 1))
+        >>> maze.get_character(1, 0)
+        '#'
+        >>> maze.get_character(2, 1)
+        'G'
+        """
+
+        if self.rat_1.row == row and self.rat_1.col == col:
+            return self.rat_1.symbol
+        elif self.rat_2.row == row and self.rat_2.col == col:
+            return self.rat_2.symbol
+        else:
+            return self.maze[row][col]
+
+    def move(self, rat, vert, horiz):
+        """(Maze, Rat, int, int) -> bool
+
+        Move Rat in Maze in the vertical direction given by vert and the
+        horizontal direction given by horiz, unless there is a wall in the way.
+        If Rat encounters a Brussels sprout, it eats it and the number of
+        sprouts in Maze decremented accordingly.
+
+        >>> rat_1 = Rat('J', 1, 1)
+        >>> rat_2 = Rat('G', 2, 1)
+        >>> maze = Maze([['#','#','#'],\
+                         ['#','.','#'],\
+                         ['#','@','#'],\
+                         ['#','#','#']],\
+                         rat_1,\
+                         rat_2)
+        >>> rat_1.num_sprouts_eaten
+        0
+        >>> maze.move(rat_1, DOWN, NO_CHANGE)
+        True
+        >>> rat_1.num_sprouts_eaten
+        1
+        >>> maze.num_sprouts_left
+        0
+        """
+
+        new_row = rat.row + vert
+        new_col = rat.col + horiz
+        if self.is_wall(new_row, new_col):
+            return False
+        else:
+            rat.set_location(new_row, new_col)
+            if self.maze[new_row][new_col] == SPROUT:
+                rat.eat_sprout()
+                self.maze[new_row][new_col] = HALL
+                self.num_sprouts_left -= 1
+            return True
+
+    def __str__(self):
+        """(Maze) -> str
+
+        Return string representation of maze, e.g.
+        >>> rat_1 = Rat('J', 1, 1)
+        >>> rat_2 = Rat('G', 2, 1)
+        >>> maze = Maze([['#','#','#','#'],\
+                         ['#','.','.','#'],\
+                         ['#','.','@','#'],\
+                         ['#','#','#','#']],\
+                         rat_1,\
+                         rat_2)
+        >>> print(maze)
+        ####
+        #J.#
+        #G@#
+        ####
+        J at (1, 1) ate 0 sprouts.
+        G at (2, 1) ate 0 sprouts.
+        """
+
+        result = ''
+        for row in range(len(self.maze)):
+            for col in range(len(self.maze[row])):
+                result += self.get_character(row, col)
+            result += '\n'
+        result += self.rat_1.__str__()
+        result += '\n'
+        result += self.rat_2.__str__()
+
+        return result
 
 if __name__ == '__main__':
     import doctest
